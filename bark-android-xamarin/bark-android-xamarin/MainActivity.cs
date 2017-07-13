ea42f8815d4ea4624b8d8ce06f5069c222a39a28
Khuram Chaudhry <kchaud2@pride.hofstra.edu>
2017-07-13 17:19:17 -0400

    docs:Added Layout files

--- bark-android-xamarin/bark-android-xamarin/MainActivity.cs
+++ bark-android-xamarin/bark-android-xamarin/MainActivity.cs
@@ -7,7 +7,7 @@ namespace barkandroidxamarin
     [Activity(Label = "bark-android-xamarin", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        //int count = 1;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -18,9 +18,9 @@ namespace barkandroidxamarin
 
             // Get our button from the layout resource,
             // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.myButton);
+            //Button button = FindViewById<Button>(Resource.Id.myButton);
 
-            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+            //button.Click += delegate { button.Text = $"{count++} clicks!"; };
         }
     }
 }
